0016ca537a555c014047496d7154cd86448fe49e
SongCodeMaster <lssyl_@naver.com>
2025-10-28 06:10:13 +0900

    feat: CORS 설정

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.youtube_routes import youtube_router
 from db.db import create_db_and_tables  
 import uvicorn
 
 app = FastAPI(title="InfluROI YouTube API")
+
+# CORS 설정 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # origins 목록에 있는 주소에서의 요청을 허용
+    allow_credentials=True,    # 쿠키 허용
+    allow_methods=["*"],       # 모든 메소드(GET, POST, PUT 등) 허용
+    allow_headers=["*"],       # 모든 헤더 허용
+)
 
 # (추가) 서버 시작 시 DB와 테이블 생성
 @app.on_event("startup")
